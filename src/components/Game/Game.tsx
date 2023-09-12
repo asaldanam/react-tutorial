7d9964ed9e0482f7aea268b34836bcc9ebe79e2a
Sara <scalmunia@gmail.com>
2023-09-12 13:08:50 +0200

    ej 3 hecho

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,9 +5,10 @@ export default function Game() {
   // Guardar en el estado la matriz de 9 elementos
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [ascendingOrder, setAscendingOrder] = useState(true); // Estado para controlar el orden ascendente/descendente
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
-  const historyLessLast = history.slice(0, -1); //Copiar el array history, menos el último elemento  
+  // const historyLessLast = history.slice(0, -1); //Copiar el array history, menos el último elemento  
   const lastMoveDescription = currentMove > 0 ? `Estás en el movimiento #${currentMove}` : "Comienza el juego"; // Agregar el último movimiento como texto
 
   function handlePlay(nextSquares: any) {
@@ -20,22 +21,33 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  const moves = historyLessLast.map((_, move) => {
-    const description = move > 0 ? 'Ir al movimiento #' + move : 'Ir al inicio del juego';
-    // let description;
-    // if (move > 0) {
-    //   description = 'Ir al movimiento #' + move;
-    // } else {
-    //   description = 'Ir al inicio del juego';
-    // }
+  function toggleOrder() {
+    setAscendingOrder(!ascendingOrder);
+  }
 
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
+  const moves = history
+    .slice(0, -1)
+    .map((_, move) => {
+      const description = move > 0 ? 'Ir al movimiento #' + move : 'Ir al inicio del juego';
+      // let description;
+      // if (move > 0) {
+      //   description = 'Ir al movimiento #' + move;
+      // } else {
+      //   description = 'Ir al inicio del juego';
+      // }
 
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    });
+
+  // Incluye el último movimiento en reversedMoves
+  const reversedMoves = [...moves, <li key="lastMove">{lastMoveDescription}</li>];
+
+  // Invierte el orden de reversedMoves si es necesario
+  const orderedMoves = ascendingOrder ? reversedMoves : [...reversedMoves].reverse();
 
 
   return (
@@ -45,11 +57,8 @@ export default function Game() {
           <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
         </div>
         <div className="game-info">
-          <ol>
-            {moves}
-            <li>{lastMoveDescription}</li>
-          </ol>
-          {/* <button onClick={}>Invertir orden</button> */}
+          <button onClick={toggleOrder}>Cambiar orden ({ascendingOrder ? "Ascendente" : "Descendente"})</button>
+          <ol>{orderedMoves}</ol>
         </div>
       </div>
     </>
